1d493f8de60d93c51df05d7d8fb6b242d91b1205
asus <hassan.shokoohimatin@gmail.com>
2019-12-03 04:27:55 +0330

    create class address'

---
+++ src/com/company/Address.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Address {
+    private String postalCode;
+    private String city;
+    private String zone;
+    private String street;
+    private String alley;
+}
