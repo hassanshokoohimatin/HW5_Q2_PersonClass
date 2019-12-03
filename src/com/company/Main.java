b01a9415e26463e9284fd1773ae2be22865dd4c6
asus <hassan.shokoohimatin@gmail.com>
2019-12-03 04:53:58 +0330

    modifying Person class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,5 +12,14 @@ public class Main {
 
     public static List<Person> addToList (){
 
+        Address address1 = new Address("11111","Tehran","4","Hengam","Sharifi");
+        Address address2 = new Address("22222","Tehran","8","Madani","Samangan");
+        Address address3 = new Address("33333","Tehran","1","Bahonar","Saba");
+        Address address4 = new Address("44444","Tehran","2","Valiasr","Ziba");
+        Address address5 = new Address("55555","Tehran","10","Navvab","Aghaghia");
+
+
+
+
     }
 }
--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,10 +1,15 @@
 package com.company;
 
 public class Person {
-    private int age;
     private String firstNmae;
     private String lastName;
+    private int age;
     private Address address;
 
-
+    public Person(String firstNmae, String lastName, int age, Address address) {
+        this.firstNmae = firstNmae;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
+    }
 }
